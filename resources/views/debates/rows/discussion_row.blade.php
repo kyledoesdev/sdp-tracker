b2b13d7e52d079963e3546ded40a474c2941a408
Kyle Evangelisto <kyleevangelisto@gmail.com>
2023-06-27 00:00:39 -0400

    i am so sorry laravel - static implementation because I am poor

--- resources/views/debates/rows/discussion_row.blade.php
+++
@@ -1,8 +0,0 @@
-<td class="pt-3"><i class="fa fa-times"></i></td>
-<td class="pt-3"><i class="fa fa-check"></i></td>
-<td class="pt-3"><i class="fa fa-minus"></i></td>
-<td class="pt-3"><i class="fa fa-minus"></i></td>
-@if ($debate->season == 1)
-    <td class="pt-3"><i class="fa fa-minus"></i></td>
-@endif
-<td class="pt-3"><i class="fa fa-minus"></i></td>
--- resources/views/errors/500.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Kyle probably fucked something up, or didn\'t pay Bezos. Send him a message and let him know something broke.'))
