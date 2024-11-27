7ee1605f67fcb4cf3328885e5c8e17ebc4e10986
kyledoesdev <kyledoesdev@gmail.com>
2024-11-27 13:39:10 -0500

    fix yml for js

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('home page loads', function () {
+it('loads the home page', function () {
     $this->get('/')->assertStatus(302); /* redirects home */
     $this->get('/home')->assertStatus(302); /* redirects to ?season=3 */
     $this->get('/home?season=3')->assertOk(); /* Home */
