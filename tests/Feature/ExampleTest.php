0bfd48ff4e128fdae8ce549d42d47552b6a3bc1b
kyledoesdev <kyledoesdev@gmail.com>
2024-11-27 13:34:10 -0500

    fix test for sdp tracker needs

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-
-    $response->assertStatus(200);
+it('home page loads', function () {
+    $this->get('/')->assertStatus(302); /* redirects home */
+    $this->get('/home')->assertStatus(302); /* redirects to ?season=3 */
+    $this->get('/home?season=3')->assertOk(); /* Home */
 });
