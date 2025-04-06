908e120c128e3b741d6b18ad4e75384437e3f005
Kyle <55156513+kyledoesdev@users.noreply.github.com>
2025-04-06 17:38:51 -0400

    Feature/season 4 (#7)
    * Add new seasons
    
    * update test
    
    * add laravel health and developer middleware

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Spatie\Health\Commands\ScheduleCheckHeartbeatCommand;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(ScheduleCheckHeartbeatCommand::class)->everyMinute();
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Middleware\IsDeveloper;
 use App\Livewire\Episodes;
 use App\Livewire\Home;
 use App\Livewire\Stats;
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('/', fn() => redirect(route('home').'?season=4'))->name('root');
 Route::get('/home', Home::class)->name('home');
 Route::get('/stats', Stats::class)->name('stats');
 
-Route::middleware(['auth', 'verified'])->group(function() {
+Route::middleware(['auth'])->group(function() {
     Route::get('/dashboard', Episodes::class)->name('dashboard');
     Route::view('/profile', 'profile')->name('profile');
+
+    Route::middleware(IsDeveloper::class)->group(function() {
+        Route::get('health', HealthCheckResultsController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
