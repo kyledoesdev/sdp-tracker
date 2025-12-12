95630ac3c6110ce52f8e47a5822fe8a964f0c6e6
kyledoesdev <kyledoesdev@gmail.com>
2025-12-11 19:33:15 -0500

    fix layouts and tests

--- tests/Feature/PageTest.php
+++ tests/Feature/PageTest.php
@@ -1,9 +1,7 @@
 <?php
 
 it('loads the home page', function () {
-    $this->get(route('root'))->assertStatus(302); /* redirects home */
-    $this->get(route('home'))->assertStatus(302); /* redirects to ?season=3 */
-    $this->get(route('home') . '?season=4')->assertOk(); /* Home */
+    $this->get(route('home'))->assertOk(); /* Home */
 });
 
 it('loads the stats page', function() {
